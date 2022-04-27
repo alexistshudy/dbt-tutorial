8bd14a5bd66925061ee01644aa6bd3aa788d8f1c
abtshudy <abtshudy@othot.com>
2022-04-27 16:05:34 -0400

    add more columns & vars

--- models/File_Overview_RowCount.sql
+++
@@ -1,9 +0,0 @@
-select 
-    "{{ var('base_table') }}" AS [File Name]
-    , COUNT(*) AS [Number of Rows]
-FROM {{ var('base_table') }}
-UNION 
-select 
-    "{{ var('table_1') }}" AS [File Name]
-    , COUNT(*) AS [Number of Rows]
-FROM {{ var('table_1') }}
